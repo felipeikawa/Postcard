339461f13e4882ebbfad72dae1b0437e97201952
Felipe Ken Ikawa <felipeikawa@Felipes-MacBook-Pro.local>
2015-03-09 16:50:31 -0300

    Initial Commit

---
+++ xcode projects/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  xcode projects
+//
+//  Created by Felipe Ken Ikawa on 3/9/15.
+//  Copyright (c) 2015 Felipe Ken Ikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ xcode projects/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  xcode projects
+//
+//  Created by Felipe Ken Ikawa on 3/9/15.
+//  Copyright (c) 2015 Felipe Ken Ikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
